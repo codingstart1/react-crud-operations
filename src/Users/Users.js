bf088b0d69bfac77ae349255185173fd22edb7a3
MoustafaYasserGit <MOU.fci15@gmail.com>
2023-09-22 23:50:52 +0400

    finalize project

--- src/Users/Users.js
+++ src/Users/Users.js
@@ -1,31 +1,81 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import axios from 'axios';
+import { Dialog } from 'primereact/dialog';
+import ViewUser from './_viewUser';
+import AddUser from './_addUser';
+import EditUser from './_editUser';
+import { ConfirmDialog } from 'primereact/confirmdialog';
+import { confirmDialog } from 'primereact/confirmdialog';
+
 
 function Users() {
 
-    const [users,setUsersList] = useState([])
+    const [users, setUsersList] = useState([]);
+    const [showViewMode, setShowViewMode] = useState(false);
+    const [showAddMode, setShowAddMode] = useState(false);
+    const [showEditMode, setShowEditMode] = useState(false);
+    const [selectedUserId, setSelectedUserId] = useState(null)
 
     useEffect(() => {
         getAllUsers();
     }, []);
-    
 
-    const getAllUsers = async () =>{
-        try{
+
+    const getAllUsers = async () => {
+        try {
             const response = await axios.get('http://localhost:4000/users');
-            if(response){
+            if (response) {
                 setUsersList(response.data);
             }
         }
-        catch (e){
-
+        catch (e) {
+            console.log(e)
         }
     }
 
-    const actionTemplae = (value)=>{
-        return <span>{value.id}</span>
+    const actionsTemplate = (rowDate) => {
+        return (
+            <>
+                <button className='btn btn-success' onClick={() => {
+                    setSelectedUserId(rowDate.id)
+                    setShowViewMode(true)
+                }}>
+                    <i className='pi pi-eye'></i>
+                </button>
+                <button className='btn btn-primary' onClick={() => {
+                    setSelectedUserId(rowDate.id)
+                    setShowEditMode(true)
+                }}>
+                    <i className='pi pi-file-edit'></i>
+                </button>
+                <button className='btn btn-danger' onClick={() => deleteUserConfirm(rowDate.id)}>
+                    <i className='pi pi-trash'></i>
+                </button>
+            </>
+        )
+    }
+
+    const deleteUserConfirm = (userId) => {
+        confirmDialog({
+            message: 'Are you sure you want to delete this user?',
+            header: 'Confirmation',
+            icon: 'pi pi-trash',
+            accept: () => deleteUser(userId),
+        });
+    }
+
+    const deleteUser = async (userId) =>{
+        try{
+            const response = await axios.delete('http://localhost:4000/users/' + userId);
+            if(response){
+                getAllUsers();
+            }
+        }
+        catch (e){
+            console.log(e)
+        }
     }
 
     return (
@@ -37,16 +87,53 @@ function Users() {
                 <h3>We will use React, Primereact, Json-server and Axios</h3>
 
                 <div className='users-list'>
+                    <div className='addNewUser'>
+                        <button className='btn btn-success' onClick={() => setShowAddMode(true)}>
+                            Add New User <i className='pi pi-plus'></i>
+                        </button>
+                    </div>
                     <DataTable value={users}>
                         <Column field="name" header="Name"></Column>
                         <Column field="username" header="Username"></Column>
                         <Column field="email" header="Email Adress"></Column>
                         <Column field="phone" header="Phone Number"></Column>
                         <Column field="website" header="Website"></Column>
-                        <Column field="action" header="Action" body={actionTemplae}></Column>
+                        <Column header="Actions" body={actionsTemplate}></Column>
                     </DataTable>
                 </div>
             </div>
+            <Dialog header="View User Data"
+                visible={showViewMode}
+                style={{ width: '70vw' }}
+                onHide={() => setShowViewMode(false)}>
+
+                <ViewUser userId={selectedUserId} />
+            </Dialog>
+
+            <Dialog header="Add New User"
+                visible={showAddMode}
+                style={{ width: '70vw' }}
+                onHide={() => setShowAddMode(false)}>
+
+                <AddUser setUserAdded={() => {
+                    setShowAddMode(false);
+                    getAllUsers();
+                }} />
+            </Dialog>
+
+            <Dialog header="Edit Exist User"
+                visible={showEditMode}
+                style={{ width: '70vw' }}
+                onHide={() => setShowEditMode(false)}>
+
+                <EditUser userId={selectedUserId} setUserEdited={() => {
+                    setShowEditMode(false);
+                    getAllUsers();
+                }} />
+            </Dialog>
+
+            <ConfirmDialog />
+
         </div>
     )
 }
